710d95ad56f7c46638f5c60779fdcb06e60aa61f
yannmm <yannmm@foxmail.com>
2026-09-17 14:37:44 +0800

    feat: trim vscode settings

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -60,15 +60,6 @@
       "[art]" = {
         "editor.formatOnSave" = true;
       };
-      "emmet.includeLanguages" = {
-        "javascript" = "javascriptreact";
-        "erb" = "html";
-        "ruby" = "html";
-      };
-      "tailwindCSS.includeLanguages" = {
-        "plaintext" = "html";
-      };
-      "emmet.triggerExpansionOnTab" = true;
       "[python]" = {
         "editor.formatOnSave" = true;
         "editor.formatOnType" = true;
@@ -78,21 +69,6 @@
       "files.associations" = {
         "*.html.erb" = "erb";
         "*.css" = "tailwindcss";
-      };
-      "[erb]" = {
-        "editor.defaultFormatter" = "aliariff.vscode-erb-beautify";
-        "editor.formatOnSave" = true;
-      };
-      "vscode-erb-beautify.customEnvVar" = {
-        "LC_ALL" = "en_US.UTF-8";
-      };
-      "[ruby]" = {
-        "editor.defaultFormatter" = "rubocop.vscode-rubocop";
-        "editor.formatOnSave" = true;
-        "editor.tabSize" = 2;
-        "editor.insertSpaces" = true;
-        "editor.semanticHighlighting.enabled" = true;
-        "editor.formatOnType" = false;
       };
       "editor.renderWhitespace" = "none";
       "security.workspace.trust.untrustedFiles" = "open";
@@ -108,15 +84,7 @@
       "workbench.colorTheme" = "Codemos Modern (Dark)";
       "terminal.integrated.inheritEnv" = true;
       "terminal.integrated.defaultProfile.osx" = "zsh";
-      "rufo.exe" = "rufo";
-      "rufo.args" = [];
-      "rufo.useBundler" = false;
-      "typescript.updateImportsOnFileMove.enabled" = "always";
       "github.copilot.nextEditSuggestions.enabled" = true;
-      "amazonQ.suppressPrompts" = {
-        "amazonQChatDisclaimer" = true;
-        "amazonQSessionConfigurationMessage" = true;
-      };
       "chat.tools.terminal.autoApprove" = {
         "flutter" = true;
         "rspec" = true;
@@ -128,9 +96,7 @@
         "plaintext" = false;
         "markdown" = false;
         "scminput" = false;
-        "ruby" = true;
       };
-      "rubocop.mode" = "onlyRunGlobally";
       "chat.mcp.gallery.enabled" = true;
       "chat.viewSessions.orientation" = "stacked";
       "claudeCode.preferredLocation" = "panel";
